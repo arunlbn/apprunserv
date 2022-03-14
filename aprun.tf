13ce7387109f8eb333212bb05cbc990ec777b5fc
arunlbn <97378356+arunlbn@users.noreply.github.com>
2022-03-14 22:23:58 +0530

    Create aprun.tf

---
+++ aprun.tf
@@ -0,0 +1,36 @@
+resource "aws_apprunner_service" "example" {
+  service_name = "example"
+
+  source_configuration {
+    authentication_configuration {
+      connection_arn = aws_apprunner_connection.example.arn
+    }
+    code_repository {
+      code_configuration {
+        code_configuration_values {
+          build_command = "pip install -r requirements.txt"
+          port          = "8080"
+          runtime       = "PYTHON_3"
+          start_command = "python server.py"
+        }
+        configuration_source = "API"
+      }
+      repository_url = "https://github.com/arunlbn/apprunner-test"
+      source_code_version {
+        type  = "BRANCH"
+        value = "main"
+      }
+    }
+  }
+
+  network_configuration {
+    egress_configuration {
+      egress_type       = "VPC"
+      vpc_connector_arn = aws_apprunner_vpc_connector.connector.arn
+    }
+  }
+
+  tags = {
+    Name = "example-apprunner-service"
+  }
+}
