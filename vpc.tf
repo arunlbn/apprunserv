b11a97b1235be3ac2c1d5f3c0539ba0b17d1ca14
arunlbn <97378356+arunlbn@users.noreply.github.com>
2022-03-16 21:16:04 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,8 +5,8 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1b", "us-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  azs             = ["us-west-2b", "us-west-2c"]
+ 
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = false
