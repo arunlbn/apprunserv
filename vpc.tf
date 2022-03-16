25562646e0a50ef3efa00597e1ab60230725d6a2
arunlbn <97378356+arunlbn@users.noreply.github.com>
2022-03-16 22:01:26 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "app-vpc-region2"
   cidr = "10.0.8.0/21"
   azs             = ["us-west-2a", "us-west-2b"]
-  ppublic_subnets = ["10.0.8.0/24", "10.0.9.0/24"]
+  public_subnets = ["10.0.8.0/24", "10.0.9.0/24"]
  
   enable_dns_hostnames = true
   enable_dns_support   = true
